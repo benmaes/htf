08fff7b10df384945e0c7456c88ad8a8bc5f7e6a
Ben Maes <ben.maes@c4j.be>
2017-11-24 09:29:34 +0100

    project setup

---
+++ src/main/java/be/bewire/htf/HackTheFutureApplication.java
@@ -0,0 +1,12 @@
+package be.bewire.htf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackTheFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackTheFutureApplication.class, args);
+	}
+}
